92ae84dcbba77935b51b60bd23017ad6670b575a
Yevhen Alexandrov <zhenya.bobyr@icloud.com>
2020-09-28 22:22:33 +0300

    Added package file.

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.2.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YPImagePicker",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v12)],
+    products: [ .library(name: "YPImagePicker", targets: ["YPImagePicker"]) ],
+    dependencies: [
+        .package(url: "https://github.com/freshOS/Stevia", .exact("5.1.0")),
+        .package(url: "https://github.com/HHK1/PryntTrimmerView", .exact("4.0.2"))
+    ],
+    targets: [
+        .target(name: "YPImagePicker", dependencies: ["Stevia", "PryntTrimmerView"]),
+        .testTarget(name: "YPImagePickerTests", dependencies: ["YPImagePicker"]),
+    ]
+)
